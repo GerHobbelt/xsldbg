4acf8075e08872a57cf2b3e9cf0b18fd7dcdc16c
Keith Isdale <keithisdale@gmail.com>
2009-09-05 08:22:53 +0000

    Add file missing from last commit

---
+++ src/libxsldbg/options_win32.cpp
@@ -0,0 +1,54 @@
+
+/***************************************************************************
+                          options_win32.c  -  win32 specific option functions
+                             -------------------
+    begin                : Tue Jan 29 2002
+    copyright            : (C) 2001 by Keith Isdale
+    email                : k_isdale@tpg.com.au
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <libxml/parser.h>
+#include "stdlib.h"
+#include <string.h>
+#include "xsldbg.h"
+#include "options.h"
+
+  /**
+   * optionsPlatformInit:
+   *
+   * Intialize the platform specific options module
+   *
+   *  This is a platform specific interface
+   *
+   * Returns 1 if sucessful
+   *         0 otherwise  
+   */
+int
+optionsPlatformInit(void)
+{
+    return 1;
+}
+
+
+  /**
+   * optionsPlatformFree:
+   *
+   * Free memory used by the platform specific options module
+   *
+   *  This is a platform specific interface
+   *
+   */
+void
+optionsPlatformFree(void)
+{
+    /* empty */
+}
